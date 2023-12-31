475fd92b9295c1ab896b2f79f4a38a2a80fd59d6
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2023-12-31 14:28:29 -0600

    feature: ReplaceCurrentAuctions() and GetCurrentAuctions()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package auctions_db
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-pg/pg/v10"
 )
 
@@ -29,6 +30,38 @@ type Auction struct {
 	ItemID         int      `pg:"item_id,pk"`
 	Interval       int16    `pg:"interval,pk"`
 	Timestamp      int32    `pg:"timestamp,pk"`
+	Quantity       int32    `pg:"quantity"`
+	Min            int32    `pg:"min,use_zero"`
+	Max            int32    `pg:"max,use_zero"`
+	P05            int32    `pg:"p05,use_zero"`
+	P10            int32    `pg:"p10,use_zero"`
+	P25            int32    `pg:"p25,use_zero"`
+	P50            int32    `pg:"p50,use_zero"`
+	P75            int32    `pg:"p75,use_zero"`
+	P90            int32    `pg:"p90,use_zero"`
+}
+
+type CurrentAuction struct {
+	tableName      struct{} `pg:"current_auctions"`
+	RealmID        int16    `pg:"realm_id,pk"`
+	AuctionHouseID int16    `pg:"auction_house_id,pk"`
+	ItemID         int      `pg:"item_id,pk"`
+	Quantity       int32    `pg:"quantity"`
+	Min            int32    `pg:"min,use_zero"`
+	Max            int32    `pg:"max,use_zero"`
+	P05            int32    `pg:"p05,use_zero"`
+	P10            int32    `pg:"p10,use_zero"`
+	P25            int32    `pg:"p25,use_zero"`
+	P50            int32    `pg:"p50,use_zero"`
+	P75            int32    `pg:"p75,use_zero"`
+	P90            int32    `pg:"p90,use_zero"`
+}
+
+type currentAuctionsTemp struct {
+	tableName      struct{} `pg:"current_auctions_temp"`
+	RealmID        int16    `pg:"realm_id,pk"`
+	AuctionHouseID int16    `pg:"auction_house_id,pk"`
+	ItemID         int      `pg:"item_id,pk"`
 	Quantity       int32    `pg:"quantity"`
 	Min            int32    `pg:"min,use_zero"`
 	Max            int32    `pg:"max,use_zero"`
@@ -154,6 +187,38 @@ func (database *Database) GetAuctions(interval int16, realmId int16, auctionHous
 	return auctions, nil
 }
 
+func (database *Database) GetCurrentAuctions(realmId int16, auctionHouseId int16, orderBy string, direction string, limit int16) ([]CurrentAuction, error) {
+	var orderByQuery string
+	if orderBy == "p50" {
+		orderByQuery = "p50"
+	} else {
+		orderByQuery = "quantity"
+	}
+
+	var directionQuery string
+	if direction == "desc" {
+		directionQuery = "DESC"
+	} else {
+		directionQuery = "ASC"
+	}
+
+	query := fmt.Sprintf(`
+		SELECT item_id, quantity, min, max, p05, p10, p25, p50, p75, p90
+		FROM current_auctions
+		WHERE realm_id = ? AND auction_house_id = ?
+		ORDER BY %s %s
+		LIMIT ?
+	`, orderByQuery, directionQuery)
+
+	var currentAuctions []CurrentAuction
+	_, err := database.db.Query(&currentAuctions, query, realmId, auctionHouseId, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return currentAuctions, nil
+}
+
 func (database *Database) InsertAuctions(auctions []*Auction) error {
 	for i := 0; i < len(auctions); i += database.BatchSize {
 		end := i + database.BatchSize
@@ -232,7 +297,71 @@ func (database *Database) ReplacePriceDistributions(priceDistributions []*PriceD
 	}
 
 	_, err = tx.Exec("TRUNCATE TABLE price_distributions_temp")
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
 
+	return nil
+}
+
+func (database *Database) ReplaceCurrentAuctions(currentAuctions []*CurrentAuction) error {
+	cat := make([]*currentAuctionsTemp, len(currentAuctions))
+	for i, v := range currentAuctions {
+		cat[i] = &currentAuctionsTemp{
+			RealmID:        v.RealmID,
+			AuctionHouseID: v.AuctionHouseID,
+			ItemID:         v.ItemID,
+			Quantity:       v.Quantity,
+			Min:            v.Min,
+			Max:            v.Max,
+			P05:            v.P05,
+			P10:            v.P10,
+			P25:            v.P25,
+			P50:            v.P50,
+			P75:            v.P75,
+			P90:            v.P90,
+		}
+	}
+
+	for i := 0; i < len(cat); i += database.BatchSize {
+		end := i + database.BatchSize
+		if end > len(cat) {
+			end = len(cat)
+		}
+		batch := cat[i:end]
+		_, err := database.db.Model(&batch).Insert()
+		if err != nil {
+			return err
+		}
+	}
+
+	tx, err := database.db.Begin()
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec("ALTER TABLE current_auctions RENAME TO current_auctions_temp2")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec("ALTER TABLE current_auctions_temp RENAME TO current_auctions")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec("ALTER TABLE current_auctions_temp2 RENAME TO current_auctions_temp")
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	_, err = tx.Exec("TRUNCATE TABLE price_distributions_temp")
 	err = tx.Commit()
 	if err != nil {
 		tx.Rollback()
